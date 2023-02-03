09423fff9fd0e8b96c18c3868c731334e5b260e5
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-02-03 13:07:47 +0500

    Displaying initials of strings

---
+++ Lab 2 Tasks/Task2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.String;
+public class Task2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str=sc.nextLine();
+        int count=0;
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==' ')
+            {
+                 count=i;
+            }
+        }
+        System.out.println("Initials are : "+str.charAt(0)+ " " +str.charAt(count+1));
+    }
+}
